4f49624aa1822656be478567b657c9dd50ca35fc
nolepguy <157486295+nolepguy@users.noreply.github.com>
2025-01-26 19:12:44 +0800

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,5 +1,5 @@
 (function() {
-    fetch('https://raw.githubusercontent.com/yeorinhieut/novel-dl/main/script.js')
+    fetch('https://raw.githubusercontent.com/nolepguy/novel-dl/refs/heads/main/script.js')
         .then(response => {
             if (!response.ok) {
                 throw new Error(`Failed to fetch script: ${response.statusText}`);
